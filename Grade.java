753e1a722376f9833d725f7c137bb30eee789840
David <dandave225@gmail.com>
2026-06-05 03:46:23 +0100

    Add grade.java file

--- Grade.java
+++ Grade.java
@@ -15,9 +15,31 @@ import java.util.Scanner;
   int subjects = input.nextInt();
 
 
-  int[] scores = new int[students];
+  int[][] scores = new int[students][subjects];
 
+  for (int index = 0; index < students; index++) { 
+      System.out.println("Entering scores for student " + (index + 1));
 
+ for (int subindex = 0; subindex < subjects; subindex++) {
+      System.out.print("Enter score for subject " + (subindex + 1) + ": ");
+
+  int score = input.nextInt();
+
+  while (score < 0 || score > 100) {
+       System.out.print("Score must be 0 - 100. Try again ");
+                       score = input.nextInt();
+
+       }
+
+    scores[index][subindex] = score;
+}
+ 
+ System.out.println("Saving >>>>>>>>>>>>>>>>>>>>>>>");
+ System.out.println("Saved Succesfully");
+
+ }
+
+     
 
 
      }
